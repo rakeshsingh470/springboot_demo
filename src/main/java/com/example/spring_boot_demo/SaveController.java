d5196781763928dc6d856aebc6d0c0d1b2064560
ad <rakesh.singh470@gmail.com>
2023-03-16 17:46:00 +0530

    commit1

---
+++ src/main/java/com/example/spring_boot_demo/SaveController.java
@@ -0,0 +1,11 @@
+package com.example.spring_boot_demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Hello")
+public class SaveController {
+
+
+}
